cd6de3a137f38acaae3178d12feaa72ea5a2a633
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-17 00:19:19 +0800

    Pseudocode

---
+++ user_input_find_the_highest_number.py
@@ -0,0 +1,25 @@
+#ask user to input 5 variables
+
+#compare the variables
+
+#compare var1 to var2
+#if var1 is greater than or equal to var2
+#the highest variable would be var1
+#if not, the highest would be var2
+
+#compare var2 to var3
+#if var2 is greater than or equal to var3
+#the highest variable would be var2
+#if not, the highest would be var3
+
+#compare var3 to var4
+#if var3 is greater than or equal to var4
+#the highest variable would be var3
+#if not, the highest would be var4
+
+#compare var4 to var5
+#if var4 is greater than or equal to var5
+#the highest variable would be var4
+#if not, the highest would be var5
+
+#print the highest variable
