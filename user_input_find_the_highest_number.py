d4b5ef28c89f86c3193dbbf4778e2105e25636fa
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-17 00:55:23 +0800

    a finished user input program that finds the highest number among the variables given

--- user_input_find_the_highest_number.py
+++ user_input_find_the_highest_number.py
@@ -1,30 +1,34 @@
 #ask user to input 5 variables
-var1 = float(input("Enter first variable : "))
-var2 = float(input("Enter second variable : "))
-var3 = float(input("Enter third variable : "))
-var4 = float(input("Enter fourth variable : "))
-var5 = float(input("Enter fifth variable : "))
+var1 = float(input("Enter first variable: "))
+var2 = float(input("Enter second variable: "))
+var3 = float(input("Enter third variable: "))
+var4 = float(input("Enter fourth variable: "))
+var5 = float(input("Enter fifth variable: "))
 
 #compare the variables
-
 #compare var1 to var2
-#if var1 is greater than or equal to var2
-#the highest variable would be var1
-#if not, the highest would be var2
+if var1 >= var2:
+    highest = var1
+else:
+    highest = var2
 
 #compare var2 to var3
-#if var2 is greater than or equal to var3
-#the highest variable would be var2
-#if not, the highest would be var3
+if var2 >= var3:
+    highest = var2
+else:
+    highest = var3
 
 #compare var3 to var4
-#if var3 is greater than or equal to var4
-#the highest variable would be var3
-#if not, the highest would be var4
+if var3 >= var4:
+    highest = var3
+else:
+    highest = var4
 
 #compare var4 to var5
-#if var4 is greater than or equal to var5
-#the highest variable would be var4
-#if not, the highest would be var5
+if var4 >= var5:
+    highest = var4
+else:
+    highest = var5 
 
 #print the highest variable
+print(f'The highest variable is: {highest}')
